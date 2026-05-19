f30957479fef5493b4f53181c55bdc5a08b4e877
Heba020 <Heba.salem.hegab@gmail.com>
2026-05-19 16:52:07 +0200

    Fix server auth token handling

--- src/AuthToken.ts
+++ src/AuthToken.ts
@@ -1,11 +1,14 @@
-import { getServerSession } from "next-auth";
+'use server'
 
-import { authOptions } from "./scheme/Auth";
+import { cookies } from "next/headers";
+import { decode } from "next-auth/jwt";
 
 export async function getAuthToken() {
+  const cookieName = process.env.NODE_ENV === "production" ? '__Secure-next-auth.session-token':'next-auth.session-token'
+    const authToken = (await cookies()).get(cookieName)?.value
+    const token = await decode({ token: authToken, secret: process.env.NEXTAUTH_SECRET! })
 
-  const session =
-    await getServerSession(authOptions);
+return token?.token;
 
-  return (session as any)?.accessToken;
+
 }
